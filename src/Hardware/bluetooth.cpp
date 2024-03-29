9b6de5f7fb04d3482ba5648b15bce0df19f557db
lethanner <50079644+lethanner@users.noreply.github.com>
2024-03-29 22:33:40 +0400

    ESP32 I2S pins remap

--- src/Hardware/bluetooth.cpp
+++ src/Hardware/bluetooth.cpp
@@ -6,7 +6,7 @@ void LTDABT::init()
     i2s_pin_config_t my_pin_config = {
         .bck_io_num = 26,
         .ws_io_num = 25,
-        .data_out_num = 23,
+        .data_out_num = 27,
         .data_in_num = I2S_PIN_NO_CHANGE
     };
 
