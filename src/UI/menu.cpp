037f8bc01e53c1a8081d7d896a4cad369c62e23c
lethanner <50079644+lethanner@users.noreply.github.com>
2024-04-28 12:51:45 +0400

    комменты

--- src/UI/menu.cpp
+++ src/UI/menu.cpp
@@ -1,9 +1,10 @@
 #include "UI.h"
 
+// обработчик меню канала (стандартного)
 void LTDAUI::_menu_channel_h(byte sel)
 {
     switch (sel) {
-    case 0:
+    case 0: // send to monitor
         setMonitorDataFeed(onScreenChannels[onScreenChSelect]);
         break;
     }
@@ -12,19 +13,21 @@ void LTDAUI::_menu_channel_h(byte sel)
     createMixingConsole(selectedGroup);
 }
 
+// обработчик меню канала master
 void LTDAUI::_menu_master_h(byte sel)
 {
     switch (sel) {
-    case 0:
+    case 0: // send to monitor
         setMonitorDataFeed(onScreenChannels[onScreenChSelect]);
         createMixingConsole(selectedGroup); // выход
         break;
-    case 1:
+    case 1: // bassboost
         DSP.toggleBassBoost();
         break;
     }
 }
 
+// обработчик меню группы каналов
 void LTDAUI::_menu_group_h(byte sel)
 {
     switch (sel) {
@@ -37,6 +40,7 @@ void LTDAUI::_menu_group_h(byte sel)
     }
 }
 
+// обработчик меню sends on fader (выбор пункта назначения посылов)
 void LTDAUI::_menu_SOFdest_h(byte sel)
 {
     createMixingConsole(selectedGroup, sel);
