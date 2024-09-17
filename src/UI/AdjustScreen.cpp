97a2cde673b2363f072cba0364d1be188d9a713d
lethanner <50079644+lethanner@users.noreply.github.com>
2024-09-17 13:15:36 +0400

    отобр. отрицательных значений AdjustScreen

--- src/UI/AdjustScreen.cpp
+++ src/UI/AdjustScreen.cpp
@@ -17,9 +17,18 @@ void LEDUI::AdjustScreen::render()
     display.invertText(0);
     printValue(*_value, _unit, 0, 24, true);
     // полосочка
-    // TODO: сделать так, чтобы эта дичь могла рисовать эту полоску и для отрицательных значений
-    byte x_end = map(*_value, __min, __max, 10, 116);
-    display.rect(10, 44, x_end, 49, OLED_FILL);
+    if (__min < 0) { // для значений, которые могут быть отрицательными
+        if (*_value > 0) { // если текущее значение больше нуля, рисуем полоску вправо
+            byte x_end = map(*_value, 0, __max, 63, 116);
+            display.rect(63, 44, x_end, 49, OLED_FILL);
+        } else { // иначе рисуем полоску влево
+            byte x_start = map(*_value, __min, 0, 10, 63);
+            display.rect(x_start, 44, 63, 49, OLED_FILL);
+        }
+    } else { // если значение не может быть отрицательным, делаем всё как обычно
+        byte x_end = map(*_value, __min, __max, 10, 116);
+        display.rect(10, 44, x_end, 49, OLED_FILL);
+    }
 }
 
 void LEDUI::AdjustScreen::onHold()
