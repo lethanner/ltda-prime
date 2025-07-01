7756322cc54ce3d9b24ea56ae35fb8d6026348d0
lethanner <50079644+lethanner@users.noreply.github.com>
2025-07-01 22:09:22 +0400

    PCB pins remap

--- src/config.h
+++ src/config.h
@@ -6,15 +6,20 @@
 #define EB_HOLD_TIME 400
 
 #define BLUETOOTH_NAME "LTDA Prime"
-#define STR_DEV_INFO "Prime v0.5-alpha"
+#define STR_DEV_INFO "Prime pre-v0.6"
 
 /* Аппаратные настройки */
 #define DSP_I2C_ADDRESS 0x38
 #define OLED_I2C_ADDRESS 0x3C
 
 #define SHIFT_DAT 15
-#define SHIFT_CLK 16
-#define SHIFT_LAT 17
-#define CTRL_KEY 5
-#define CTRL_S1 19
-#define CTRL_S2 18
+#define SHIFT_CLK 4
+#define SHIFT_LAT 16
+#define CTRL_KEY 0
+#define CTRL_S1 39
+#define CTRL_S2 36
+#define T_SENSOR 14
+#define V_SENSOR 32
+#define FAN_PWM 12
+#define AMP_FAULT 34
+#define AMP_CLIP 35
